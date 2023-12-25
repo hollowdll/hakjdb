2f12ec83b439f457956a8b003d71a1f3bfe33971
hollowdll <pixerchat@gmail.com>
2023-12-25 19:07:47 +0200

    db get and set

--- db.go
+++ db.go
@@ -2,8 +2,25 @@ package kvdb
 
 import "sync"
 
+// Database containing key-value pairs of data.
 type Database struct {
-	name  string
+	Name  string
 	data  map[DatabaseKey]DatabaseValue
 	mutex sync.RWMutex
+}
+
+// Retrieves a value using a key.
+func (db *Database) Get(key DatabaseKey) DatabaseValue {
+	db.mutex.RLock()
+	defer db.mutex.RUnlock()
+
+	return db.data[key]
+}
+
+// Sets a value using a key.
+func (db *Database) Set(key DatabaseKey, value DatabaseValue) {
+	db.mutex.Lock()
+	defer db.mutex.Unlock()
+
+	db.data[key] = value
 }
