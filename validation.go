62c12b81c64487d025a8c1d6e13bfc546ddf0add
hollowdll <pixerchat@gmail.com>
2023-12-26 19:21:21 +0200

    update validation

--- validation.go
+++ validation.go
@@ -10,15 +10,22 @@ func isEmpty(input string) bool {
 }
 
 func containsWhitespace(input string) bool {
-	return strings.ContainsAny(input, " \t\n\r\v\f")
+	return strings.ContainsAny(input, " .\t\n\r\v\f")
+}
+
+func containsInvalidSymbol(input string) bool {
+	return strings.ContainsAny(input, ".")
 }
 
 func validateDatabaseName(name string) error {
 	if isEmpty(name) {
-		return errors.New("Database name cannot be empty")
+		return errors.New("Database name is empty")
 	}
 	if containsWhitespace(name) {
-		return errors.New("Database name cannot contain whitespace")
+		return errors.New("Database name contains whitespace")
+	}
+	if containsInvalidSymbol(name) {
+		return errors.New("Database name contains invalid symbol")
 	}
 
 	return nil
