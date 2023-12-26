cdb07a0f9f37f8acda884ed256d019a63d214df8
hollowdll <pixerchat@gmail.com>
2023-12-26 22:19:51 +0200

    init log entry

--- logger.go
+++ logger.go
@@ -2,7 +2,9 @@ package kvdb
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"time"
 )
 
 const logsDirName string = "logs"
@@ -10,6 +12,31 @@ const allLogsFile string = "all_logs.log"
 
 // Logger manages read and write operations to log files.
 type Logger struct{}
+
+type logEntry struct {
+	createdAt time.Time
+	logType   logType
+	content   string
+}
+
+func (l *logEntry) String() string {
+	return fmt.Sprintf("[%s] [%s] %s", l.createdAt.UTC(), l.logType, l.content)
+}
+
+type logType int
+
+const (
+	logTypeInfo logType = iota
+)
+
+func (l logType) String() string {
+	switch l {
+	case logTypeInfo:
+		return "Info"
+	default:
+		return "Invalid LogType"
+	}
+}
 
 // Logs info to log file.
 func (l Logger) LogInfo(info string) error {
