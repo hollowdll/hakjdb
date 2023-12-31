fe616f64cfe49e82bcd078204f09779ae88a7310
hollowdll <pixerchat@gmail.com>
2024-01-01 00:20:37 +0200

    update validation

--- validation.go
+++ validation.go
@@ -2,23 +2,34 @@ package kvdb
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 )
+
+// Database name maximum length in bytes.
+const dbNameMaxSize int = 32
 
 func isEmpty(input string) bool {
 	return len(input) == 0
 }
 
-func containsValidCharacters(input string) bool {
+func dbNameTooLong(name string) bool {
+	return len(name) > dbNameMaxSize
+}
+
+func dbNamecontainsValidCharacters(name string) bool {
 	pattern := regexp.MustCompile("^[A-Za-z0-9-_]+$")
-	return pattern.MatchString(input)
+	return pattern.MatchString(name)
 }
 
 func validateDatabaseName(name string) error {
 	if isEmpty(name) {
 		return errors.New("database name is empty")
 	}
-	if !containsValidCharacters(name) {
+	if dbNameTooLong(name) {
+		return fmt.Errorf("database name is too long (max %d bytes)", dbNameMaxSize)
+	}
+	if !dbNamecontainsValidCharacters(name) {
 		return errors.New("database name contains invalid characters")
 	}
 
