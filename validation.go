b3cc5807532186c3f37adca5ee5c70d952155cc0
hollowdll <pixerchat@gmail.com>
2024-01-06 20:47:46 +0200

    refactor validations

--- validation.go
+++ validation.go
@@ -1,21 +1,22 @@
 package kvdb
 
 import (
-	"errors"
-	"fmt"
 	"regexp"
+	"strings"
+
+	kvdberrors "github.com/hollowdll/kvdb/errors"
 )
 
 const (
 	// DbNameMaxSize is the maximum length of database name in bytes.
 	DbNameMaxSize int = 32
 	// DbKeyMaxSize is the maximum length of database key in bytes.
-	DbKeyMaxSize int = 32
+	DbKeyMaxSize int = 1024
 )
 
-// isEmpty returns true if input is too long.
-func isEmpty(input string) bool {
-	return len(input) == 0
+// isBlank returns true if input is blank.
+func isBlank(input string) bool {
+	return len(strings.TrimSpace(input)) == 0
 }
 
 // isTooLong returns true if input is longer than target.
@@ -31,24 +32,17 @@ func dbNamecontainsValidCharacters(name string) bool {
 	return pattern.MatchString(name)
 }
 
-// dbKeycontainsValidCharacters checks if database key
-// contains valid characters by matching it against a regexp.
-func dbKeycontainsValidCharacters(key DatabaseKey) bool {
-	pattern := regexp.MustCompile("^[A-Za-z0-9-_:]+$")
-	return pattern.MatchString(string(key))
-}
-
 // validateDatabaseName validates database name.
 // Returns error if validation error is matched.
 func validateDatabaseName(name string) error {
-	if isEmpty(name) {
-		return errors.New("database name is empty")
+	if isBlank(name) {
+		return kvdberrors.ErrDatabaseNameRequired
 	}
 	if isTooLong(name, DbNameMaxSize) {
-		return fmt.Errorf("database name is too long (max %d bytes)", DbNameMaxSize)
+		return kvdberrors.ErrDatabaseNameTooLong
 	}
 	if !dbNamecontainsValidCharacters(name) {
-		return errors.New("database name contains invalid characters")
+		return kvdberrors.ErrDatabaseNameInvalid
 	}
 
 	return nil
@@ -57,14 +51,11 @@ func validateDatabaseName(name string) error {
 // validateDatabaseKey validates database key.
 // Returns error if validation error is matched.
 func validateDatabaseKey(key DatabaseKey) error {
-	if isEmpty(string(key)) {
-		return errors.New("key is empty")
+	if isBlank(string(key)) {
+		return kvdberrors.ErrDatabaseKeyRequired
 	}
 	if isTooLong(string(key), DbKeyMaxSize) {
-		return fmt.Errorf("key is too long (max %d bytes)", DbKeyMaxSize)
-	}
-	if !dbKeycontainsValidCharacters(key) {
-		return errors.New("key contains invalid characters")
+		return kvdberrors.ErrDatabaseKeyTooLong
 	}
 
 	return nil
