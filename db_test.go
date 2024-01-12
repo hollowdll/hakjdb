88af76a29f64d3e909aa5ee87e59e9473bf983bb
hollowdll <pixerchat@gmail.com>
2024-01-12 20:23:51 +0200

    test db key count

--- db_test.go
+++ db_test.go
@@ -17,3 +17,29 @@ func TestCreateDatabase(t *testing.T) {
 		t.Fatalf("expected db with name %s but got %s", expectedDb.GetName(), db.GetName())
 	}
 }
+
+func TestGetDatabaseKeyCount(t *testing.T) {
+	db := newDatabase("test")
+	count := db.GetKeyCount()
+	if count != 0 {
+		t.Fatalf("key count should be 0 but got %d", count)
+	}
+
+	err := db.SetString("key1", "value1")
+	if err != nil {
+		t.Fatalf("error setting string value")
+	}
+	count = db.GetKeyCount()
+	if count != 1 {
+		t.Fatalf("key count should be 1 but got %d", count)
+	}
+
+	err = db.SetString("key2", "value2")
+	if err != nil {
+		t.Fatalf("error setting string value")
+	}
+	count = db.GetKeyCount()
+	if count != 2 {
+		t.Fatalf("key count should be 2 but got %d", count)
+	}
+}
