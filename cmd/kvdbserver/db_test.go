45552efc4d8886553d639d40b6671a8b8253347a
hollowdll <pixerchat@gmail.com>
2024-01-13 22:32:26 +0200

    add testify

--- cmd/kvdbserver/db_test.go
+++ cmd/kvdbserver/db_test.go
@@ -6,15 +6,15 @@ import (
 
 	main "github.com/hollowdll/kvdb/cmd/kvdbserver"
 	"github.com/hollowdll/kvdb/proto/kvdbserver"
+	"github.com/stretchr/testify/assert"
 )
 
+// TODO: test cases
 func TestCreateDatabase(t *testing.T) {
 	server := main.NewServer()
 
 	request := &kvdbserver.CreateDatabaseRequest{DbName: "test"}
 	_, err := server.CreateDatabase(context.Background(), request)
 
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoErrorf(t, err, "error creating database: %s", err)
 }
