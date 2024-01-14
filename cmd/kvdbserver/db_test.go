4b2b7d22b874fd2bb76c25cccca3ea880b24eedd
hollowdll <pixerchat@gmail.com>
2024-01-14 16:11:51 +0200

    tests for rpc GetAllDatabases

--- cmd/kvdbserver/db_test.go
+++ cmd/kvdbserver/db_test.go
@@ -52,7 +52,7 @@ func TestCreateDatabase(t *testing.T) {
 		request := &kvdbserver.CreateDatabaseRequest{DbName: dbName}
 		response, err := server.CreateDatabase(context.Background(), request)
 
-		assert.Error(t, err, "expected no error")
+		assert.Error(t, err, "expected error")
 		assert.Nil(t, response, "expected response to be nil")
 
 		st, ok := status.FromError(err)
@@ -60,4 +60,49 @@ func TestCreateDatabase(t *testing.T) {
 		assert.Equal(t, true, ok, "expected ok")
 		assert.Equal(t, codes.InvalidArgument, st.Code(), "expected status = %s; got = %s", codes.InvalidArgument, st.Code())
 	})
+}
+
+func TestGetAllDatabases(t *testing.T) {
+	t.Run("NoDatabases", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		expected := 0
+		request := &kvdbserver.GetAllDatabasesRequest{}
+		response, err := server.GetAllDatabases(context.Background(), request)
+
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+		assert.NotNil(t, response, "expected response to be non-nil")
+		assert.Equalf(t, expected, len(response.DbNames), "expected databases = %d; got = %d", expected, len(response.DbNames))
+	})
+
+	t.Run("MultipleDatabases", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+
+		dbs := []string{"db0", "db1", "db2"}
+		for _, db := range dbs {
+			request := &kvdbserver.CreateDatabaseRequest{DbName: db}
+			_, err := server.CreateDatabase(context.Background(), request)
+			assert.NoErrorf(t, err, "expected no error; error = %s", err)
+		}
+
+		request := &kvdbserver.GetAllDatabasesRequest{}
+		response, err := server.GetAllDatabases(context.Background(), request)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+		assert.NotNil(t, response, "expected response to be non-nil")
+		assert.Equalf(t, len(dbs), len(response.DbNames), "expected databases = %d; got = %d", len(dbs), len(response.DbNames))
+
+		for _, db := range response.DbNames {
+			assert.Equalf(t, true, stringInSlice(db, dbs), "expected database name %s to be in %v", db, dbs)
+		}
+	})
+}
+
+func stringInSlice(target string, slice []string) bool {
+	for _, elem := range slice {
+		if elem == target {
+			return true
+		}
+	}
+	return false
 }
