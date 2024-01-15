61e1835b92235b3cd3dbcb8217c63158b595c29f
hollowdll <pixerchat@gmail.com>
2024-01-15 20:53:42 +0200

    add tests for rpc GetString

--- cmd/kvdbserver/storage_test.go
+++ cmd/kvdbserver/storage_test.go
@@ -37,7 +37,7 @@ func TestSetString(t *testing.T) {
 
 		md := metadata.Pairs("wrong-key", dbName)
 		ctx := metadata.NewIncomingContext(context.Background(), md)
-		ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
 		defer cancel()
 
 		request := &kvdbserver.SetStringRequest{Key: "key1", Value: "value1"}
@@ -58,7 +58,7 @@ func TestSetString(t *testing.T) {
 
 		md := metadata.Pairs(common.GrpcMetadataKeyDbName, dbName)
 		ctx := metadata.NewIncomingContext(context.Background(), md)
-		ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
 		defer cancel()
 
 		request := &kvdbserver.SetStringRequest{Key: "key1", Value: "value1"}
@@ -72,7 +72,7 @@ func TestSetString(t *testing.T) {
 		assert.Equal(t, codes.NotFound, st.Code(), "expected status = %s; got = %s", codes.NotFound, st.Code())
 	})
 
-	t.Run("DatabaseExists", func(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
 		server := main.NewServer()
 		server.DisableLogger()
 		dbName := "db0"
@@ -83,7 +83,7 @@ func TestSetString(t *testing.T) {
 
 		md := metadata.Pairs(common.GrpcMetadataKeyDbName, dbName)
 		ctx := metadata.NewIncomingContext(context.Background(), md)
-		ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
 		defer cancel()
 
 		request := &kvdbserver.SetStringRequest{Key: "key1", Value: "value1"}
@@ -103,7 +103,7 @@ func TestSetString(t *testing.T) {
 
 		md := metadata.Pairs(common.GrpcMetadataKeyDbName, dbName)
 		ctx := metadata.NewIncomingContext(context.Background(), md)
-		ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
 		defer cancel()
 
 		request := &kvdbserver.SetStringRequest{Key: "      ", Value: "value1"}
@@ -115,5 +115,113 @@ func TestSetString(t *testing.T) {
 		assert.NotNil(t, st, "expected status to be non-nil")
 		assert.Equal(t, true, ok, "expected ok")
 		assert.Equal(t, codes.InvalidArgument, st.Code(), "expected status = %s; got = %s", codes.InvalidArgument, st.Code())
+	})
+}
+
+func TestGetString(t *testing.T) {
+	t.Run("MissingMetadata", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+
+		request := &kvdbserver.GetStringRequest{Key: "key1"}
+		response, err := server.GetString(context.Background(), request)
+		assert.Error(t, err, "expected error")
+		assert.Nil(t, response, "expected response to be nil")
+
+		st, ok := status.FromError(err)
+		assert.NotNil(t, st, "expected status to be non-nil")
+		assert.Equal(t, true, ok, "expected ok")
+		assert.Equal(t, codes.InvalidArgument, st.Code(), "expected status = %s; got = %s", codes.InvalidArgument, st.Code())
+	})
+
+	t.Run("MissingDatabaseInMetadata", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		dbName := "db0"
+
+		md := metadata.Pairs("wrong-key", dbName)
+		ctx := metadata.NewIncomingContext(context.Background(), md)
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+
+		request := &kvdbserver.GetStringRequest{Key: "key1"}
+		response, err := server.GetString(ctx, request)
+		assert.Error(t, err, "expected error")
+		assert.Nil(t, response, "expected response to be nil")
+
+		st, ok := status.FromError(err)
+		assert.NotNil(t, st, "expected status to be non-nil")
+		assert.Equal(t, true, ok, "expected ok")
+		assert.Equal(t, codes.InvalidArgument, st.Code(), "expected status = %s; got = %s", codes.InvalidArgument, st.Code())
+	})
+
+	t.Run("DatabaseNotFound", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		dbName := "db0"
+
+		md := metadata.Pairs(common.GrpcMetadataKeyDbName, dbName)
+		ctx := metadata.NewIncomingContext(context.Background(), md)
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+
+		request := &kvdbserver.GetStringRequest{Key: "key1"}
+		response, err := server.GetString(ctx, request)
+		assert.Error(t, err, "expected error")
+		assert.Nil(t, response, "expected response to be nil")
+
+		st, ok := status.FromError(err)
+		assert.NotNil(t, st, "expected status to be non-nil")
+		assert.Equal(t, true, ok, "expected ok")
+		assert.Equal(t, codes.NotFound, st.Code(), "expected status = %s; got = %s", codes.NotFound, st.Code())
+	})
+
+	t.Run("SuccessKeyNotFound", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		dbName := "db0"
+
+		requestCreate := &kvdbserver.CreateDatabaseRequest{DbName: dbName}
+		_, err := server.CreateDatabase(context.Background(), requestCreate)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+
+		md := metadata.Pairs(common.GrpcMetadataKeyDbName, dbName)
+		ctx := metadata.NewIncomingContext(context.Background(), md)
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+
+		request := &kvdbserver.GetStringRequest{Key: "key1"}
+		response, err := server.GetString(ctx, request)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+		assert.NotNil(t, response, "expected response to be non-nil")
+		assert.Equalf(t, false, response.Found, "expected found = %v; got = %v", false, response.Found)
+		assert.Equalf(t, "", response.Value, "expected empty string; got = %v", response.Value)
+	})
+
+	t.Run("SuccessKeyFound", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		dbName := "db0"
+		expectedValue := "value1"
+
+		requestCreate := &kvdbserver.CreateDatabaseRequest{DbName: dbName}
+		_, err := server.CreateDatabase(context.Background(), requestCreate)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+
+		md := metadata.Pairs(common.GrpcMetadataKeyDbName, dbName)
+		ctx := metadata.NewIncomingContext(context.Background(), md)
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+
+		requestSet := &kvdbserver.SetStringRequest{Key: "key1", Value: expectedValue}
+		_, err = server.SetString(ctx, requestSet)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+
+		requestGet := &kvdbserver.GetStringRequest{Key: "key1"}
+		response, err := server.GetString(ctx, requestGet)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+		assert.NotNil(t, response, "expected response to be non-nil")
+		assert.Equalf(t, true, response.Found, "expected found = %v; got = %v", true, response.Found)
+		assert.Equalf(t, expectedValue, response.Value, "expected value = %s; got = %v", expectedValue, response.Value)
 	})
 }
