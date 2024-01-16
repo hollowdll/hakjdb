038a9185008dd002d40fb6a2d5203c9904be1502
hollowdll <pixerchat@gmail.com>
2024-01-16 21:27:44 +0200

    add tests for rpc DeleteKey

--- cmd/kvdbserver/storage_test.go
+++ cmd/kvdbserver/storage_test.go
@@ -167,7 +167,7 @@ func TestGetString(t *testing.T) {
 		assert.NoErrorf(t, err, "expected no error; error = %s", err)
 		assert.NotNil(t, response, "expected response to be non-nil")
 		assert.Equalf(t, false, response.Found, "expected found = %v; got = %v", false, response.Found)
-		assert.Equalf(t, "", response.Value, "expected empty string; got = %v", response.Value)
+		assert.Equalf(t, "", response.Value, "expected empty string; got = %s", response.Value)
 	})
 
 	t.Run("SuccessKeyFound", func(t *testing.T) {
@@ -190,6 +190,97 @@ func TestGetString(t *testing.T) {
 		assert.NoErrorf(t, err, "expected no error; error = %s", err)
 		assert.NotNil(t, response, "expected response to be non-nil")
 		assert.Equalf(t, true, response.Found, "expected found = %v; got = %v", true, response.Found)
-		assert.Equalf(t, expectedValue, response.Value, "expected value = %s; got = %v", expectedValue, response.Value)
+		assert.Equalf(t, expectedValue, response.Value, "expected value = %s; got = %s", expectedValue, response.Value)
+	})
+}
+
+func TestDeleteKey(t *testing.T) {
+	t.Run("MissingMetadata", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+
+		request := &kvdbserver.DeleteKeyRequest{Key: "key1"}
+		response, err := server.DeleteKey(context.Background(), request)
+		assert.Error(t, err, "expected error")
+		assert.Nil(t, response, "expected response to be nil")
+
+		st, ok := status.FromError(err)
+		assert.NotNil(t, st, "expected status to be non-nil")
+		assert.Equal(t, true, ok, "expected ok")
+		assert.Equal(t, codes.InvalidArgument, st.Code(), "expected status = %s; got = %s", codes.InvalidArgument, st.Code())
+	})
+
+	t.Run("MissingDatabaseInMetadata", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		dbName := "db0"
+		ctxMd := metadata.NewIncomingContext(context.Background(), metadata.Pairs("wrong-key", dbName))
+
+		request := &kvdbserver.DeleteKeyRequest{Key: "key1"}
+		response, err := server.DeleteKey(ctxMd, request)
+		assert.Error(t, err, "expected error")
+		assert.Nil(t, response, "expected response to be nil")
+
+		st, ok := status.FromError(err)
+		assert.NotNil(t, st, "expected status to be non-nil")
+		assert.Equal(t, true, ok, "expected ok")
+		assert.Equal(t, codes.InvalidArgument, st.Code(), "expected status = %s; got = %s", codes.InvalidArgument, st.Code())
+	})
+
+	t.Run("DatabaseNotFound", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		dbName := "db0"
+		ctxMd := metadata.NewIncomingContext(context.Background(), metadata.Pairs(common.GrpcMetadataKeyDbName, dbName))
+
+		request := &kvdbserver.DeleteKeyRequest{Key: "key1"}
+		response, err := server.DeleteKey(ctxMd, request)
+		assert.Error(t, err, "expected error")
+		assert.Nil(t, response, "expected response to be nil")
+
+		st, ok := status.FromError(err)
+		assert.NotNil(t, st, "expected status to be non-nil")
+		assert.Equal(t, true, ok, "expected ok")
+		assert.Equal(t, codes.NotFound, st.Code(), "expected status = %s; got = %s", codes.NotFound, st.Code())
+	})
+
+	t.Run("SuccessKeyNotFound", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		dbName := "db0"
+		ctxMd := metadata.NewIncomingContext(context.Background(), metadata.Pairs(common.GrpcMetadataKeyDbName, dbName))
+
+		requestCreate := &kvdbserver.CreateDatabaseRequest{DbName: dbName}
+		_, err := server.CreateDatabase(context.Background(), requestCreate)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+
+		request := &kvdbserver.DeleteKeyRequest{Key: "key1"}
+		response, err := server.DeleteKey(ctxMd, request)
+		expected := false
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+		assert.NotNil(t, response, "expected response to be non-nil")
+		assert.Equalf(t, expected, response.Success, "expected success = %v; got = %v", expected, response.Success)
+	})
+
+	t.Run("SuccessKeyFound", func(t *testing.T) {
+		server := main.NewServer()
+		server.DisableLogger()
+		dbName := "db0"
+		ctxMd := metadata.NewIncomingContext(context.Background(), metadata.Pairs(common.GrpcMetadataKeyDbName, dbName))
+
+		requestCreate := &kvdbserver.CreateDatabaseRequest{DbName: dbName}
+		_, err := server.CreateDatabase(context.Background(), requestCreate)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+
+		requestSet := &kvdbserver.SetStringRequest{Key: "key1", Value: "v"}
+		_, err = server.SetString(ctxMd, requestSet)
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+
+		requestGet := &kvdbserver.DeleteKeyRequest{Key: "key1"}
+		response, err := server.DeleteKey(ctxMd, requestGet)
+		expected := true
+		assert.NoErrorf(t, err, "expected no error; error = %s", err)
+		assert.NotNil(t, response, "expected response to be non-nil")
+		assert.Equalf(t, expected, response.Success, "expected success = %v; got = %v", expected, response.Success)
 	})
 }
