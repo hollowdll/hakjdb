ac5fd983533b0d4998ece30ee090e87b757a8a60
hollowdll <pixerchat@gmail.com>
2024-01-19 21:39:30 +0200

    update storage integration tests

--- tests/integration/storage_test.go
+++ tests/integration/storage_test.go
@@ -14,7 +14,7 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-func TestSetAndGetString(t *testing.T) {
+func TestOverwriteAndGetString(t *testing.T) {
 	conn, err := grpc.Dial(getServerAddress(), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	require.NoErrorf(t, err, "Failed to connect to the server: %v", err)
 	defer conn.Close()
@@ -22,7 +22,8 @@ func TestSetAndGetString(t *testing.T) {
 	storageClient := kvdbserver.NewStorageServiceClient(conn)
 	dbName := "TestSetAndGetString"
 	key := "key1"
-	value := "value1"
+	value1 := "value1"
+	value2 := "value2"
 	ctxMd := metadata.NewOutgoingContext(context.Background(), metadata.Pairs(common.GrpcMetadataKeyDbName, dbName))
 	ctx, cancel := context.WithTimeout(ctxMd, 5*time.Second)
 	defer cancel()
@@ -32,17 +33,22 @@ func TestSetAndGetString(t *testing.T) {
 	require.NoErrorf(t, err, "expected no error; error = %v", err)
 	require.NotNil(t, res1)
 
-	req2 := &kvdbserver.SetStringRequest{Key: key, Value: value}
+	req2 := &kvdbserver.SetStringRequest{Key: key, Value: value1}
 	res2, err := storageClient.SetString(ctx, req2)
 	require.NoErrorf(t, err, "expected no error; error = %v", err)
 	require.NotNil(t, res2)
 
-	req3 := &kvdbserver.GetStringRequest{Key: key}
-	res3, err := storageClient.GetString(ctx, req3)
+	req3 := &kvdbserver.SetStringRequest{Key: key, Value: value2}
+	res3, err := storageClient.SetString(ctx, req3)
 	require.NoErrorf(t, err, "expected no error; error = %v", err)
 	require.NotNil(t, res3)
-	assert.Equal(t, value, res3.Value, "expected value = %s; got = %s", value, res3.Value)
-	assert.Equal(t, true, res3.Found, "expected found = %v; got = %v", true, res3.Found)
+
+	req4 := &kvdbserver.GetStringRequest{Key: key}
+	res4, err := storageClient.GetString(ctx, req4)
+	require.NoErrorf(t, err, "expected no error; error = %v", err)
+	require.NotNil(t, res4)
+	assert.Equal(t, value2, res4.Value, "expected value = %s; got = %s", value2, res4.Value)
+	assert.Equal(t, true, res4.Found, "expected found = %v; got = %v", true, res4.Found)
 }
 
 func TestSetGetDeleteString(t *testing.T) {
@@ -87,4 +93,10 @@ func TestSetGetDeleteString(t *testing.T) {
 	require.NotNil(t, res5)
 	assert.Equal(t, "", res5.Value, "expected empty string; got = %s", value, res5.Value)
 	assert.Equal(t, false, res5.Found, "expected found = %v; got = %v", false, res5.Found)
+
+	req6 := &kvdbserver.DeleteKeyRequest{Key: key}
+	res6, err := storageClient.DeleteKey(ctx, req6)
+	require.NoErrorf(t, err, "expected no error; error = %v", err)
+	require.NotNil(t, res6)
+	assert.Equal(t, false, res6.Success, "expected success = %v; got = %v", false, res6.Success)
 }
