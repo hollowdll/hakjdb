dafdb807df3390b17c6c3404dcd5eac35d466440
hollowdll <pixerchat@gmail.com>
2024-02-02 20:16:56 +0200

    tests for IsCorrectServerPassword

--- cmd/kvdbserver/server/auth_test.go
+++ cmd/kvdbserver/server/auth_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/hollowdll/kvdb/cmd/kvdbserver/server"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSetServerPassword(t *testing.T) {
@@ -22,6 +23,30 @@ func TestSetServerPassword(t *testing.T) {
 		password := strings.Repeat("a", 73)
 
 		err := credentialStore.SetServerPassword([]byte(password))
+		assert.Error(t, err, "expected error")
+	})
+}
+
+func TestIsCorrectServerPassword(t *testing.T) {
+	t.Run("CorrectPassword", func(t *testing.T) {
+		credentialStore := server.NewInMemoryCredentialStore()
+		password := "pass123?"
+
+		err := credentialStore.SetServerPassword([]byte(password))
+		require.NoErrorf(t, err, "expected no error; error = %v", err)
+
+		err = credentialStore.IsCorrectServerPassword([]byte(password))
+		assert.NoErrorf(t, err, "expected no error; error = %v", err)
+	})
+
+	t.Run("IncorrectPassword", func(t *testing.T) {
+		credentialStore := server.NewInMemoryCredentialStore()
+		password := "pass123!!"
+
+		err := credentialStore.SetServerPassword([]byte(password))
+		require.NoErrorf(t, err, "expected no error; error = %v", err)
+
+		err = credentialStore.IsCorrectServerPassword([]byte("pass123?"))
 		assert.Error(t, err, "expected error")
 	})
 }
