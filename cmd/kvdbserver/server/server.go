bec5ab29f062933a70e65457289137d5918745aa
hollowdll <pixerchat@gmail.com>
2024-03-15 21:09:33 +0200

    add RPC prefix for server log messages

--- cmd/kvdbserver/server/server.go
+++ cmd/kvdbserver/server/server.go
@@ -168,12 +168,12 @@ func getOsInfo() (string, error) {
 
 // GetServerInfo returns information about the server.
 func (s *Server) GetServerInfo(ctx context.Context, req *kvdbserver.GetServerInfoRequest) (res *kvdbserver.GetServerInfoResponse, err error) {
-	s.logger.Debug("Attempt to get server info")
+	s.logger.Debug("GetServerInfo: attempt to get server info")
 	defer func() {
 		if err != nil {
-			s.logger.Errorf("Failed to get server info: %s", err)
+			s.logger.Errorf("GetServerInfo: failed to get server info: %v", err)
 		} else {
-			s.logger.Debug("Get server info success")
+			s.logger.Debug("GetServerInfo: get server info success")
 		}
 	}()
 
@@ -202,12 +202,12 @@ func (s *Server) GetServerInfo(ctx context.Context, req *kvdbserver.GetServerInf
 
 // GetLogs reads server logs from the log file and returns them.
 func (s *Server) GetLogs(ctx context.Context, req *kvdbserver.GetLogsRequest) (res *kvdbserver.GetLogsResponse, err error) {
-	s.logger.Debug("Attempt to get server logs")
+	s.logger.Debug("GetLogs: attempt to get server logs")
 	defer func() {
 		if err != nil {
-			s.logger.Errorf("Failed to get server logs: %s", err)
+			s.logger.Errorf("GetLogs: failed to get server logs: %v", err)
 		} else {
-			s.logger.Debug("Get server logs success")
+			s.logger.Debug("GetLogs: get server logs success")
 		}
 	}()
 
@@ -217,7 +217,7 @@ func (s *Server) GetLogs(ctx context.Context, req *kvdbserver.GetLogsRequest) (r
 	if !s.logFileEnabled {
 		return nil, status.Errorf(codes.FailedPrecondition, "%s: enable server log file to get logs", kvdberrors.ErrLogFileNotEnabled.Error())
 	}
-	s.logger.Debug("Log file is enabled")
+	s.logger.Debug("GetLogs: log file is enabled")
 
 	lines, err := common.ReadFileLines(s.logFilePath)
 	if err != nil {
