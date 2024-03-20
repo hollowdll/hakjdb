ec5685cf401bd74e6d6c9598cce0a6c93b597959
hollowdll <pixerchat@gmail.com>
2024-03-20 21:36:33 +0200

    add kvdb-cli command to get the data type of a key

---
+++ cmd/kvdb-cli/cmd/key_type.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hollowdll/kvdb/cmd/kvdb-cli/client"
+	"github.com/hollowdll/kvdb/internal/common"
+	"github.com/hollowdll/kvdb/proto/kvdbserver"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc/metadata"
+)
+
+var cmdGetKeyType = &cobra.Command{
+	Use:   "keytype [key]",
+	Short: "Get the data type of a key",
+	Long:  "Get the data type of a key",
+	Args:  cobra.MatchAll(cobra.ExactArgs(1)),
+	Run: func(cmd *cobra.Command, args []string) {
+		getKeyType(args[0])
+	},
+}
+
+func init() {
+	cmdGetKeyType.Flags().StringVarP(&dbName, "database", "d", "", "database to use")
+}
+
+func getKeyType(key string) {
+	md := client.GetBaseGrpcMetadata()
+	if len(dbName) > 0 {
+		md.Set(common.GrpcMetadataKeyDbName, dbName)
+	}
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	ctx, cancel := context.WithTimeout(ctx, client.CtxTimeout)
+	defer cancel()
+
+	response, err := client.GrpcStorageClient.GetTypeOfKey(ctx, &kvdbserver.GetTypeOfKeyRequest{Key: key})
+	client.CheckGrpcError(err)
+
+	if response.Ok {
+		fmt.Printf("\"%s\"\n", response.KeyType)
+	} else {
+		fmt.Println(client.ValueNone)
+	}
+}
