c087527f4a82b31599d6e9e0623f52844dd912f7
hollowdll <pixerchat@gmail.com>
2024-07-28 11:28:59 +0300

    progress on config

--- cmd/kvdbserver/config/config.go
+++ cmd/kvdbserver/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"os"
 	"path/filepath"
 
 	"github.com/hollowdll/kvdb"
@@ -12,7 +13,7 @@ const (
 	dataDirName    string = "data"
 	configFileName string = "kvdbserver-config"
 	configFileType string = "yaml"
-	logFileName    string = "kvdb.log"
+	logFileName    string = "kvdbserver.log"
 
 	// EnvPrefix is the prefix that environment variables use.
 	EnvPrefix string = "KVDB"
@@ -25,11 +26,11 @@ const (
 	// ConfigKeyLogFileEnabled is the configuration key for enabling log file.
 	ConfigKeyLogFileEnabled string = "logfile_enabled"
 	// ConfigKeyTlsEnabled is the configuration key for enabling TLS.
-	ConfigKeyTlsEnabled string = "tls_enabled"
+	ConfigKeyTLSEnabled string = "tls_enabled"
 	// ConfigKeyTlsCertPath is the configuration key for TLS certificate file path.
-	ConfigKeyTlsCertPath string = "tls_cert_path"
+	ConfigKeyTLSCertPath string = "tls_cert_path"
 	// ConfigKeyTlsPrivKeyPath is the configuration key for TLS private key file path.
-	ConfigKeyTlsPrivKeyPath string = "tls_private_key_path"
+	ConfigKeyTLSPrivKeyPath string = "tls_private_key_path"
 	// ConfigKeyMaxClientConnections is the configuration key for maximum client connections.
 	ConfigKeyMaxClientConnections string = "max_client_connections"
 
@@ -42,33 +43,29 @@ const (
 
 // ServerConfig holds the server's configuration.
 type ServerConfig struct {
-	logFileEnabled       bool
-	tlsEnabled           bool
-	debugEnabled         bool
-	passwordEnabled      bool
-	defaultDB            bool
-	logFilePath          string
-	maxKeysPerDB         uint32
-	maxHashMapFields     uint32
-	portInUse            uint16
-	maxClientConnections uint32
-	tlsCertPath          string
-	tlsPrivKeyPath       string
-	dataDirPath          string
+	LogFileEnabled       bool
+	TLSEnabled           bool
+	DebugEnabled         bool
+	DefaultDB            string
+	LogFilePath          string
+	MaxKeysPerDB         uint32
+	MaxHashMapFields     uint32
+	PortInUse            uint16
+	MaxClientConnections uint32
+	TLSCertPath          string
+	TLSPrivKeyPath       string
 }
 
 // initConfig initializes server configurations.
 func LoadConfig(lg kvdb.Logger) *ServerConfig {
 	parentDir, err := common.GetExecParentDirPath()
 	if err != nil {
-		lg.Fatalf("Failed to get executable's parent directory: %v", err)
+		lg.Fatalf("Failed to get the executable's parent directory: %v", err)
 	}
 	dataDirPath, err := common.GetDirPath(parentDir, dataDirName)
 	if err != nil {
-		lg.Fatalf("Failed to get data directory: %v", err)
+		lg.Fatalf("Failed to get the data directory: %v", err)
 	}
-
-	// s.SetLogFilePath(filepath.Join(dataDirPath, logFileName))
 
 	viper.AddConfigPath(dataDirPath)
 	viper.SetConfigType(configFileType)
@@ -78,9 +75,9 @@ func LoadConfig(lg kvdb.Logger) *ServerConfig {
 	viper.SetDefault(ConfigKeyDebugEnabled, false)
 	viper.SetDefault(ConfigKeyDefaultDatabase, DefaultDatabase)
 	viper.SetDefault(ConfigKeyLogFileEnabled, false)
-	viper.SetDefault(ConfigKeyTlsEnabled, false)
-	viper.SetDefault(ConfigKeyTlsCertPath, "")
-	viper.SetDefault(ConfigKeyTlsPrivKeyPath, "")
+	viper.SetDefault(ConfigKeyTLSEnabled, false)
+	viper.SetDefault(ConfigKeyTLSCertPath, "")
+	viper.SetDefault(ConfigKeyTLSPrivKeyPath, "")
 	viper.SetDefault(ConfigKeyMaxClientConnections, common.DefaultMaxClientConnections)
 
 	viper.SetEnvPrefix(EnvPrefix)
@@ -90,5 +87,27 @@ func LoadConfig(lg kvdb.Logger) *ServerConfig {
 		lg.Fatalf("Failed to read configuration file: %v", err)
 	}
 
-	return nil
+	return &ServerConfig{
+		LogFileEnabled:       viper.GetBool(ConfigKeyLogFileEnabled),
+		TLSEnabled:           viper.GetBool(ConfigKeyTLSEnabled),
+		DebugEnabled:         viper.GetBool(ConfigKeyDebugEnabled),
+		DefaultDB:            viper.GetString(ConfigKeyDefaultDatabase),
+		LogFilePath:          filepath.Join(dataDirPath, logFileName),
+		MaxKeysPerDB:         common.DbMaxKeyCount,
+		MaxHashMapFields:     common.HashMapMaxFields,
+		PortInUse:            viper.GetUint16(ConfigKeyPort),
+		MaxClientConnections: viper.GetUint32(ConfigKeyMaxClientConnections),
+		TLSCertPath:          viper.GetString(ConfigKeyTLSCertPath),
+		TLSPrivKeyPath:       viper.GetString(ConfigKeyTLSPrivKeyPath),
+	}
+}
+
+// ShouldUsePassword returns the server password if it is set with an environment variable.
+// The returned bool is true if it is set and false if not.
+func ShouldUsePassword() (string, bool) {
+	return getEnvVar(EnvVarPassword)
+}
+
+func getEnvVar(envVar string) (string, bool) {
+	return os.LookupEnv(envVar)
 }
