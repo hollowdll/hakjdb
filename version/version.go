de36d81b506e29cda2a775140aea135ee45ebfab
hollowdll <pixerchat@gmail.com>
2024-08-20 15:38:01 +0300

    change version to 0.15.0

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 const (
 	// Version is the kvdb version.
-	Version string = "0.14.0"
+	Version string = "0.15.0"
 	// APIVersion is the kvdb API version.
 	APIVersion string = "0.3.0"
 )
