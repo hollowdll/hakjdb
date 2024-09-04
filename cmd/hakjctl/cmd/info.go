31af74e8e1d29cadf62716fa77ecb446cfa31481
hollowdll <pixerchat@gmail.com>
2024-09-04 15:11:08 +0300

    hakjctl info show if mTLS is enabled

--- cmd/hakjctl/cmd/info.go
+++ cmd/hakjctl/cmd/info.go
@@ -29,6 +29,7 @@ General
 - uptime_seconds: Server process uptime in seconds.
 - tcp_port: Server TCP/IP port.
 - tls_enabled: If TLS is enabled. Yes or no.
+- tls_client_cert_auth_enabled: If TLS client certificate authentication is enabled. Yes or no.
 - auth_enabled: If authentication is enabled. Yes or no.
 - logfile_enabled: If the log file is enabled. Yes or no.
 - debug_enabled: If debug mode is enabled. Yes or no.
@@ -79,6 +80,12 @@ func showServerInfo() {
 		info.WriteString("tls_enabled: yes\n")
 	} else {
 		info.WriteString("tls_enabled: no\n")
+	}
+
+	if res.GeneralInfo.TlsClientCertAuthEnabled {
+		info.WriteString("tls_client_cert_auth_enabled: yes\n")
+	} else {
+		info.WriteString("tls_client_cert_auth_enabled: no\n")
 	}
 
 	if res.GeneralInfo.AuthEnabled {
